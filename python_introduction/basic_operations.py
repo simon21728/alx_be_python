65c9f7214897dcca992e8695c53fde14f1fbf8e6
sewmehon bayu <simonbayu2211@gmail.com>
2024-12-19 22:26:25 -0900

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Defining the variables
+number1 = 10
+number2 = 5
+
+# Performing the arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Printing the results
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+
